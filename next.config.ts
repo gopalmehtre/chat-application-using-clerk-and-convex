ac016269a42ae0812be7df97bdbcfc6b7a8d3678
Gopal <gopalmehtre10@gmail.com>
2026-02-27 12:15:57 +0530

    chore: add next.config with image remote patterns

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      { hostname: "api.dicebear.com" },
+      { hostname: "img.clerk.com" },
+    ],
+  },
 };
 
 export default nextConfig;
